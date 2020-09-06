d6111eb33aa6bdfaba586a3d92145c8f4c3baaa9
Savon Puentes <SavonPuentes@gmail.com>
2020-09-06 00:42:30 -0500

    Finish base

--- BasicPythonScraper/spiders/m_spydr.py
+++ BasicPythonScraper/spiders/m_spydr.py
@@ -4,22 +4,25 @@ import scrapy
 class MainSpider(scrapy.Spider):
     name = "mainCrawl"
     UPC = 0
+    
     start_urls = ['https://brickseek.com/products/?search='+str(UPC)]
     
     def parse(self, response):
+        #Retrive the HTML of the search page for the item, and follow the first item link
         new_Link = response.xpath('//*[@id="main"]/div[2]/div[1]/div/div/a/@href').extract()
         yield scrapy.Request(new_Link, callback=self.item_page_parse)
         
     def item_page_parse(self, response):
-        ItemName = response.xpath('//*[@id="main"]/div/div[1]/div[2]/h2//text()').extract()
-        ItemMSRP = float(response.xpath('//*[@id="main"]/div/div[1]/div[2]/div[1]/div/div[2]//text()').extract())
+        #Retrive the items attributes from the items page
+        ItemName = response.xpath('//*[@id="main"]/div/div[1]/div[2]/h2/text()').extract()
+        ItemMSRP = float(response.xpath('//*[@id="main"]/div/div[1]/div[2]/div[1]/div/div[2]/text()').extract())
         ItemStores = {}
-        #Div for divs: '//*[@id="online-offers"]/div'
-        #Div1 price: '//*[@id="online-offers"]/div/a[1]/span[1]'
-        #Div1 name: '//*[@id="online-offers"]/div/a[1]/span[3]'
-        #Div2 price: '//*[@id="online-offers"]/div/a[2]/span[1]'
-        #Div2 name: '//*[@id="online-offers"]/div/a[2]/span[3]'
-
+        i = 1
+        while response.xpath('//*[@id="online-offers"]/div/a['+str(i)+']') is not 'None':
+             name = response.xpath('//*[@id="online-offers"]/div/a['+str(i)+']/span[3]/text()').extract()
+             ItemStores[name] = response.xpath('//*[@id="online-offers"]/div/a['+str(i)+']/span[1]/text()').extract()
+             i+=1
+        #Write data to a file
         with open('TempItemData.txt', 'wb') as f:
             f.write("Item Name: "+ItemName)
             f.write("Item MSRP: "+ItemMSRP)
--- Main.py
+++ Main.py
@@ -3,14 +3,18 @@ from scrapy.crawler import CrawlerProcess
 from BasicPythonScraper.spiders.m_spydr import MainSpider
 
 def main():
+    #Get the Universal Product Code from the user
     UPC = input("Enter the products UPC:")
+    #Create and start the web spider
     spydr = MainSpider(UPC=UPC) 
     crawler = CrawlerProcess(settings=None)
     crawler.crawl(spydr)
     crawler.start()
+    #Print the results to consol
     f = open("TempItemData.txt", "r")
     print(f.read())
     
 
 if __name__ is '__main__':
     main()
+  
